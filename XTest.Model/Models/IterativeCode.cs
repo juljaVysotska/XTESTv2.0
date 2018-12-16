94ac48a9d9f72a8f6f46f5ef842f9de92ddec392
unknown <kazzarin99@gmail.com>
2018-12-16 13:02:33 +0200

    Iterative And Greya Codes

---
+++ XTest.Model/Models/IterativeCode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XTest.Model.Models
+{
+    class IterativeCode : INotifyPropertyChanged
+    {
+        public int q { get; set; }
+        public int[][] Array { get; set; }
+        public object PropertyChanged { get; private set; }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChangedEventArgs args = new PropertyChangedEventArgs(propertyName);
+                this.PropertyChanged(this, args);
+            }
+        }
+    }
+}
