ce75e5a65e444e35243cfbd30f5b03340d152726
iSan4eZ <temnyip@gmail.com>
2018-12-23 16:57:19 +0200

    service commit

--- XTest.Model/Services/Berger.cs
+++ XTest.Model/Services/Berger.cs
@@ -6,16 +6,16 @@ using System.Threading.Tasks;
 
 namespace XTest.Model.Services
 {
-    static class Berger
+    public static class Berger
     {
-        Random rnd = new Random();
+        static Random rnd = new Random();
 
-        public string generateDecode()
+        public static string generateDecode()
         {
             return encode(generateEncode());
         }
 
-        public string generateEncode()
+        public static string generateEncode()
         {
             string result = "";
             for (int i = 0; i < rnd.Next(6, 15); i++)
@@ -25,22 +25,29 @@ namespace XTest.Model.Services
             return result;
         }
 
-        public bool isEncodedCorrectly(string task, string result)
+        public static bool isEncodedCorrectly(string task, string result)
         {
-            return true;
+            return result.Equals(encode(task));
         }
 
-        public bool isDecodedCorrectly(string task, string result)
+        public static bool isDecodedCorrectly(string task, string result)
         {
-            return true;
+            return result.Equals(decode(task));
         }
 
-        private string encode()
+        private static string encode(string input)
         {
-            return "";
+            double r = Math.Ceiling(Math.Log(input.Length, 2));
+            int count = ~input.Count(c => c.Equals("1"));
+            string addition = count.ToString();
+            while (addition.Length < r)
+            {
+                addition = "1" + addition;
+            }
+            return input + addition;
         }
 
-        private string decode()
+        private static string decode(string input)
         {
             return "";
         }
