d52495d2e84b90d692dee0f91f20f0beb04eee06
unknown <kazzarin99@gmail.com>
2018-12-23 10:40:56 +0200

    BinaryDecimal model

--- XTest.Model/Models/BinaryDecimalCode.cs
+++ XTest.Model/Models/BinaryDecimalCode.cs
@@ -1,12 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace XTest.Model.Models
 {
-    class BinaryDecimalCode
+    class BinaryDecimalCode : INotifyPropertyChanged
     {
+        private string code;
+        private int number;
+        private string encodeNumb;
+
+        public BinaryDecimalCode()
+        {
+            this.code = "";
+            this.number = 0;
+            this.encodeNumb = "";
+        }
+
+        public string Code
+        {
+            get { return code; }
+            set
+            {
+                code = value;
+                OnPropertyChanged("Code");
+            }
+        }
+
+        public string EncodeNumb
+        {
+            get { return encodeNumb; }
+            set
+            {
+                encodeNumb = value;
+                OnPropertyChanged("EncodeNumb");
+            }
+        }
+
+        public int Number
+        {
+            get { return number; }
+            set
+            {
+                number = value;
+                OnPropertyChanged("Number");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }
