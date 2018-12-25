47bfc6bfa6c18b70773faeed6bbef62200500404
Liliia Pereliotina <liliiapereliotina@gmail.com>
2018-12-25 22:48:57 +0200

    Add class result
    Add class result

---
+++ XTest.Model/Models/Result.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XTest.Model.Models
+{
+    public class Result
+    {
+        public string testName;
+        public int testsTotal;
+        public int correctTests = 0;
+        public int currentTestNumber = 1;
+
+        public Result(string testName, int testsTotal)
+        {
+            this.testName = testName;
+            this.testsTotal = testsTotal;
+        }
+    }
+}
