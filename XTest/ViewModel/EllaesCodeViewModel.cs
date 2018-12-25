d4d15c696ba76501968ed8c9f10608a8a81f675d
Liliia Pereliotina <liliiapereliotina@gmail.com>
2018-12-25 14:50:09 +0200

    add class EllaesCodeViewModel
    add class EllaesCodeViewModel

---
+++ XTest/ViewModel/EllaesCodeViewModel.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using XTest.Model.Models;
+using XTest.Model.Services;
+
+namespace XTest.ViewModel
+{
+    public class EllaesCodeViewModel
+    {
+        private RelayCommand next;
+        private int check { get; set; }
+        private int mark { get; set; }
+        private EllaesCodeService _service { get; set; }
+        public EllaesCode EllaesCode { get; set; }
+        public int[][] array;
+        private int[][] OldArray { get; set; }
+
+        public int[][] Array
+        {
+            get { return array; }
+            set
+            {
+                array = value;
+                OnPropertyChanged("Array");
+            }
+        }
+
+
+        public EllaesCodeViewModel(EllaesCodeService ellaesCodeService, EllaesCode ellaesCode)
+        {
+            check = 1;
+            mark = 0;
+            this.EllaesCode = ellaesCode;
+            this._service = ellaesCodeService;
+            OldArray = _service.GenerateArray(4, 4);
+            Array = _service.ResizeArray(OldArray);
+        }
+
+        public RelayCommand Next
+        {
+            get
+            {
+                return next ??
+                  (next = new RelayCommand(obj =>
+                  {
+                      if (check < 4)
+                      {
+                          OldArray = _service.Code(OldArray);
+                          if (_service.Equals(OldArray, Array))
+                              mark += 1;
+
+                          OldArray = _service.GenerateArray(4, 4);
+                          Array = _service.ResizeArray(OldArray);
+
+                      }
+                      else if (check == 4)
+                      {
+                          MessageBox.Show("decode");
+                          OldArray = _service.Code(OldArray);
+                          if (_service.Equals(OldArray, Array))
+                              mark += 1;
+
+                          Array = _service.GenerateArrayWithException(4, 4);
+                          OldArray = _service.FuckenCSharp(Array);
+                      }
+                      else
+                      {
+                          OldArray = _service.Decode(OldArray);
+                          if (_service.Equals(OldArray, Array))
+                              mark += 1;
+                          Array = _service.GenerateArrayWithException(4, 4);
+                          OldArray = _service.FuckenCSharp(Array);
+                      }
+                      check += 1;
+                      if (check == 9)
+                      {
+                          MessageBox.Show(mark.ToString());
+                      }
+                  }));
+            }
+
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
