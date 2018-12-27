00c67bb4d0050bc85df317318455b83f742fff9a
iSan4eZ <temnyip@gmail.com>
2018-12-28 01:51:43 +0200

    init

---
+++ XTest.Model/Services/ShennonFanoService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XTest.Model.Services
+{
+    public class ShennonFanoService
+    {
+        public Dictionary<int, double> generateMessages()
+        {
+            return null;
+        }
+
+        public bool isCalculatedCorrectly()
+        {
+            return false;
+        }
+
+        public Dictionary<int, double> calculate()
+        {
+            return null;
+        }
+    }
+}
